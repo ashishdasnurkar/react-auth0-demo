dd4356605b67a73396c91d0f26fc538fa0e14f5a
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2018-12-16 19:13:57 +0800

    Show profile link if user already logged in

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -40,4 +40,10 @@ export default class Auth {
     localStorage.setItem("id_token", authResult.idToken);
     localStorage.setItem("expires_at", expiresAt);
   }
+
+  isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    console.log("isAuthenticated", new Date().getTime() < expiresAt);
+    return new Date().getTime() < expiresAt;
+  }
 }
